caa8e404336c9808f82cd44b9c7bf5d39c05819a
jimmy-oss <jimmynjonge51@gmail.com>
2021-12-05 20:22:25 +0300

    contact list file

--- contact.py
+++ contact.py
@@ -3,11 +3,13 @@ class Contact:
     Class that generates new instances of contacts.
     """
 
-    def __init__(self, first_name, last_name, number, email):
+    contact_list = [] # Empty contact list
+
+    def __init__(self,first_name,last_name,number,email):
 
       # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
-        self.number = number
+        self.phone_number = number
         self.email = email
