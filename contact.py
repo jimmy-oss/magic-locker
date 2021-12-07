e0467eb5eb9926b459541798627aa204c62dc163
jimmy-oss <jimmynjonge51@gmail.com>
2021-12-07 18:31:14 +0300

    contact list save

--- contact.py
+++ contact.py
@@ -3,9 +3,9 @@ class Contact:
     Class that generates new instances of contacts.
     """
 
-    contact_list = [] # Empty contact list
+    contact_list = []
 
-    def __init__(self,first_name,last_name,number,email):
+    def __init__(self, first_name, last_name, number, email):
 
       # docstring removed for simplicity
 
@@ -13,3 +13,7 @@ class Contact:
         self.last_name = last_name
         self.phone_number = number
         self.email = email
+
+    def save_contact(self):
+
+        Contact.contact_list.append(self)
--- contact_test.py
+++ contact_test.py
@@ -28,6 +28,14 @@ class TestContact(unittest.TestCase):
         self.assertEqual(self.new_contact.phone_number, "0712345678")
         self.assertEqual(self.new_contact.email, "james@ms.com")
 
+    def test_save_contact(self):
+        '''
+        test_save_contact test case to test if the contact object is saved into
+         the contact list
+        '''
+        self.new_contact.save_contact()  # saving the new contact
+        self.assertEqual(len(Contact.contact_list), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
