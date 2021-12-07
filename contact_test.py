548ec7f968363c8e96c0a0bf8a1bc142f5030fdb
jimmy-oss <jimmynjonge51@gmail.com>
2021-12-07 18:36:41 +0300

    contact list save multiple contacts

--- contact_test.py
+++ contact_test.py
@@ -36,6 +36,38 @@ class TestContact(unittest.TestCase):
         self.new_contact.save_contact()  # saving the new contact
         self.assertEqual(len(Contact.contact_list), 1)
 
+    def test_save_multiple_contact(self):
+        '''
+        test_save_multiple_contact to check if we can save multiple contact
+        objects to our contact_list
+        '''
+        self.new_contact.save_contact()
+        test_contact = Contact("Test", "user", "0712345678",
+                               "test@user.com")  # new contact
+        test_contact.save_contact()
+        self.assertEqual(len(Contact.contact_list), 2)
+# setup and class creation up here
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Contact.contact_list = []
+
+# other test cases here
+
+
+def test_save_multiple_contact(self):
+    '''
+    test_save_multiple_contact to check if we can save multiple contact
+    objects to our contact_list
+    '''
+    self.new_contact.save_contact()
+    test_contact = Contact("Test", "user", "0712345678",
+                           "test@user.com")  # new contact
+    test_contact.save_contact()
+    self.assertEqual(len(Contact.contact_list), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
