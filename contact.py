537b98c86335bfdfcaa3b57895caa79a525d2f59
jimmy-oss <jimmynjonge51@gmail.com>
2022-01-16 15:35:19 +0300

    ran all the unnitests

--- contact.py
+++ contact.py
@@ -3,7 +3,7 @@ class Contact:
     Class that generates new instances of contacts.
     """
 
-    contact_list = []
+    contact_list = []  # Empty contact list
 
     def __init__(self, first_name, last_name, number, email):
 
@@ -13,7 +13,54 @@ class Contact:
         self.last_name = last_name
         self.phone_number = number
         self.email = email
+# always remember to import e.g from contact import Contact
+# Init method up here
 
     def save_contact(self):
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
 
         Contact.contact_list.append(self)
+
+    def delete_contact(self):
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        Contact.contact_list.remove(self)
+
+    @classmethod
+    def find_by_number(cls, number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
+
+    @classmethod
+    def contact_exist(cls, number):
+        '''
+        Method that checks if a contact exists from the contact list.
+        Args:
+            number: Phone number to search if it exists
+        Returns :
+            Boolean: True or false depending if the contact exists
+        '''
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return True
+
+    @classmethod
+    def display_contacts(cls):
+        '''
+        method that returns the contact list
+        '''
+        return cls.contact_list
--- contact_test.py
+++ contact_test.py
@@ -1,5 +1,5 @@
-from contact import Contact  # Importing the contact class
 import unittest  # Importing the unittest module
+from contact import Contact  # Importing the contact class
 
 
 class TestContact(unittest.TestCase):
@@ -54,19 +54,47 @@ class TestContact(unittest.TestCase):
         '''
         Contact.contact_list = []
 
+
 # other test cases here
 
 
-def test_save_multiple_contact(self):
-    '''
-    test_save_multiple_contact to check if we can save multiple contact
-    objects to our contact_list
-    '''
-    self.new_contact.save_contact()
-    test_contact = Contact("Test", "user", "0712345678",
-                           "test@user.com")  # new contact
-    test_contact.save_contact()
-    self.assertEqual(len(Contact.contact_list), 2)
+    def test_save_multiple_contact(self):
+        '''
+         test_save_multiple_contact to check if we can save multiple contact
+         objects to our contact_list
+        '''
+        self.new_contact.save_contact()
+        test_contact = Contact("Test", "user", "0712345678",
+                               "test@user.com")  # new contact
+        test_contact.save_contact()
+        self.assertEqual(len(Contact.contact_list), 2)
+# More tests above
+
+    def test_delete_contact(self):
+        '''
+       test_delete_contact to test if we can remove a contact from our contact list
+         '''
+        self.new_contact.save_contact()
+        test_contact = Contact("Test", "user", "0712345678",
+                               "test@user.com")  # new contact
+        test_contact.save_contact()
+
+        self.new_contact.delete_contact()  # Deleting a contact object
+        self.assertEqual(len(Contact.contact_list), 1)
+
+    def test_find_contact_by_number(self):
+        '''
+       test to check if we can find a contact by phone number and display information
+       '''
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Test", "user", "0711223344",
+                               "test@user.com")  # new contact
+        test_contact.save_contact()
+
+        found_contact = Contact.find_by_number("0711223344")
+
+        self.assertEqual(found_contact.email, test_contact.email)
 
 
 if __name__ == '__main__':
